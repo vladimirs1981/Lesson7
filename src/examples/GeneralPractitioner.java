4bfa1eae3bb36eaae4e62fbe621b8f229f59cc42
Vladimir Stojanovic <st.vladas@hotmail.com>
2019-06-02 17:38:57 +0200

    GeneralPractitioner

---
+++ src/examples/GeneralPractitioner.java
@@ -0,0 +1,18 @@
+package examples;
+
+public class GeneralPractitioner extends Doctor {
+
+	public GeneralPractitioner() {
+
+		super("white");
+	}
+
+	void doMedicine() {
+		System.out.println("Prescription recipes");
+	}
+
+	void makeHouseCalls() {
+		System.out.println("Doctor speaking?");
+	}
+
+}
